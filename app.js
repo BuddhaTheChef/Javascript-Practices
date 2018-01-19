5d7972ea2b8f9f45e33a816ac5a7d4f21c1674ad
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-01-18 18:21:54 -1000

    day 1 practice

---
+++ app.js
@@ -0,0 +1,83 @@
+////////////////////////////////////////////////////////////////////////////////
+// var a = 'hello world';
+//
+// function b() {
+//   console.log('CALL b!');
+// }
+//
+// b();
+//
+// console.log(a);
+
+////////////////////////////////////////////////////////////////////////////////
+// var a;
+// console.log(a);
+//
+// if(a === undefined){
+//   console.log('a is undefined');
+// }
+// else {
+//   console.log('a is defined');
+// }
+
+////////////////////////////////////////////////////////////////////////////////
+// function b() {
+//   console.log('CALL b!');
+// }
+//
+// b();
+//
+// console.log(a);
+//
+// var a = 'hello world';
+//
+// console.log(a);
+////////////////////////////////////////////////////////////////////////////////
+//Single threaded - one command at a time;
+
+//Synchronous - one at a time and in order it appears;
+
+//Invocation - running or calling a function;
+
+// function b() {
+//   var d;
+// }
+//
+// function a() {
+//  b();
+//  var c;
+// }
+//
+// a();
+// var d;
+
+////////////////////////////////////////////////////////////////////////////////
+// function b() {
+//   var myVar;
+//   console.log(myVar);
+// }
+//
+// function a() {
+//  var myVar = 2;
+//    console.log(myVar);
+// b();
+// }
+//
+// var myVar = 1;
+//   console.log(myVar);
+// a();
+//   console.log(myVar);
+////////////////////////////////////////////////////////////////////////////////
+function b() {
+  console.log(myVar);
+}
+
+function a() {
+ var myVar = 2;
+   console.log(myVar);
+b();
+}
+
+var myVar = 1;
+a();
+  
