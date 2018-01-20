4c3d542f116ed25bc97625aca37fe4f2a367152c
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-01-19 18:04:24 -1000

    Day 2 section

--- app.js
+++ app.js
@@ -81,7 +81,9 @@
 //   console.log(myVar);
 ////////////////////////////////////////////////////////////////////////////////
 //scope - where a variable is availabe lin your code;
+
 //let - same as var but you cant use it until you define the vasriable;
+
 //Asynchronous callbacks - more than one at a time
 
 // function waitThreeSeconds() {
@@ -101,3 +103,41 @@
 // console.log('finished execution');
 
 ////////////////////////////////////////////////////////////////////////////////
+// Dynamic typing - you dont tell the engine what data to holf and figures it out
+// while code is running
+
+//Primitive Types - undefined, null, string, number, boolean, symbol
+
+//operator- specific function that is syntacfully written diffrent(usually takes
+// two parameters and return one result)
+
+//Operator precednce- which operator gets called first (PEMDAS)
+
+//Operator associativity- what order a operator function gets called in
+//(left to right) or (right to left)
+
+// var a = 2,  b= 3, c = 4;
+//
+// a = b = c;
+//
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+//ALL LOG TO 4
+
+////////////////////////////////////////////////////////////////////////////////
+
+//Coercion - converting a value from one type to another
+
+//var a = 'heelo' + 'world';
+//var a = 1 + '2' => 12
+
+// console.log(1 < 2 < 3) => true
+// console.log(3 < 2 < 1) => true "because of left to right associativity"
+
+//false == 0 => true
+//null == 0 => false
+//null < 1 => true
+//""== 0 => true
+//""== false => true
